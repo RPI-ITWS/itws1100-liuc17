a322a0a3633378dcb7b1f98b1315c1c2b9ce5798
carinaal <liucarina888@gmail.com>
2024-11-04 19:37:31 -0500

    Update script.js

--- quiz2/script.js
+++ quiz2/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
-    // Load the JSON data and display it when the submit button is clicked
     $('#submitButton').on('click', function () {
         $.getJSON('names.json', function (data) {
-            // Create an ordered list to display names
             let nameList = '<ol>';
             data.names.forEach(function (name) {
                 nameList += `<li class="name-item">${name}</li>`;
