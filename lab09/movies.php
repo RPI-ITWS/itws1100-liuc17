62c9ca0016845b339298035aae4deee6b6e5d294
carinaal <liucarina888@gmail.com>
2024-11-11 23:22:42 -0500

    lab9 commit #5

--- lab09/movies.php
+++ lab09/movies.php
@@ -1,54 +1,49 @@
-<?php
-include('includes/init.inc.php'); // include the DOCTYPE and opening tags
-include('includes/functions.inc.php'); // functions
+<?php 
+  include('includes/init.inc.php'); 
+  include('includes/functions.inc.php'); 
 ?>
-<title>PHP & MySQL - ITWS</title>
+<title>PHP &amp; MySQL - ITWS</title>   
 
 <?php include('includes/head.inc.php'); ?>
 
-<h1>PHP & MySQL</h1>
-
+<h1>PHP &amp; MySQL</h1>
+   
 <?php include('includes/menubody.inc.php'); ?>
 
 <h2>Movies & Actors</h2>
 <p>Below is a list of movies and their corresponding actors:</p>
+
 <table>
-    <tr><th>Movie Title</th><th>Actor</th></tr>
-    <?php
-    $dbOk = true;
-    @$db = new mysqli('localhost', 'root', 'root', 'iitF23');
-    if ($db->connect_error) {
-        echo '<div class="messages">Could not connect to the database. Error: ' . $db->connect_errno . ' - ' . $db->connect_error . '</div>';
-        $dbOk = false;
-    }
-    
-    if ($dbOk) {
-        // SQL query to join movies, actors, and movie_actor tables
-        $query = 'SELECT movies.title, actors.first_name, actors.last_name 
-                  FROM movies
-                  JOIN movie_actor ON movies.movieid = movie_actor.movie_id
-                  JOIN actors ON actors.actorid = movie_actor.actor_id
-                  ORDER BY movies.title';
+  <tr>
+    <th>Movie Title</th>
+    <th>Actor</th>
+  </tr>
 
-        $result = $db->query($query);
-        
-        if ($result) {
-            // Loop through each result and output it in a table row
-            while ($row = $result->fetch_assoc()) {
-                echo '<tr>';
-                echo '<td>' . htmlspecialchars($row['title']) . '</td>';
-                echo '<td>' . htmlspecialchars($row['first_name'] . ' ' . $row['last_name']) . '</td>';
-                echo '</tr>';
-            }
-            $result->free();
-        } else {
-            echo '<tr><td colspan="2">No records found or query failed.</td></tr>';
-        }
+<?php
+  @$db = new mysqli('localhost', 'root', 'root', 'iitF23');
+
+  if ($db->connect_error) {
+    echo '<div class="messages">Could not connect to the database. Error: ';
+    echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+  } else {
+    $query = "SELECT movies.title, actors.first_name, actors.last_name 
+              FROM movies 
+              JOIN movie_actor ON movies.movieid = movie_actor.movie_id 
+              JOIN actors ON actors.actorid = movie_actor.actor_id 
+              ORDER BY movies.title";
+
+    $result = $db->query($query);
+
+    while ($row = $result->fetch_assoc()) {
+      echo "<tr><td>" . htmlspecialchars($row['title']) . "</td>";
+      echo "<td>" . htmlspecialchars($row['first_name']) . " " . htmlspecialchars($row['last_name']) . "</td></tr>";
     }
 
-    // Close the database connection
+    $result->free();
     $db->close();
-    ?>
+  }
+?>
+
 </table>
 
 <?php include('includes/foot.inc.php'); ?>
